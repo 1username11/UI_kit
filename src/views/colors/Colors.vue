57bb1a8d48faaa5b79aad89484e4906496cae218
aider <auder31101997@gmail.com>
2023-06-27 19:18:27 +0300

    opacity problem

--- src/views/colors/Colors.vue
+++ src/views/colors/Colors.vue
@@ -10,20 +10,33 @@
 
     <div class="flex space-x-12 mt-8">
       <div
-        v-for="(colorSquare, idx) in colorSquaresContentItem.content"
+        v-for="colorSquare in colorSquaresContentItem.content"
         :key="colorSquare"
       >
         <div class="flex flex-col items-center">
           <div
             class="w-[124px] h-[115px] rounded-[20px]"
-            :class="colorSquaresContentItem.title === 'Primary' ?
-              squarePrimaryColors[idx] :
-              squareSecondaryColors[idx]"
+            :class="[squaresColors[colorSquare as string], colorSquare === '#ffffff' ? 'white-square-shadow' : '']"
           />
-          <div>{{ colorSquare }}</div>
+          <div class="mt-2">{{ colorSquare }}</div>
         </div>
       </div>
     </div>
+  </div>
+
+  <div class="flex">
+    <div
+      v-for="(opacity, index) in opacities"
+      :key="index"
+      :class="[
+        `${squaresColors['#024242']}`,
+        `bg-opacity-${opacity}`,
+        'w-[124px]',
+        'h-[115px]',
+        'rounded-[20px]',
+        'mt-2'
+      ]"
+    />
   </div>
 </template>
 
@@ -31,7 +44,7 @@
 const colorSquaresContent = ref([
   {
     title: 'Primary',
-    content: ['#024242', '#A5CF4C', '#D99F59', '#889EA6', '#fff']
+    content: ['#024242', '#A5CF4C', '#D99F59', '#889EA6', '#ffffff']
   },
   {
     title: 'Secondary',
@@ -39,8 +52,19 @@ const colorSquaresContent = ref([
   }
 ])
 
-const squarePrimaryColors = ref(['bg-darkGreen', 'bg-green', 'bg-orange', 'bg-darkGray', 'bg-white'])
-const squareSecondaryColors = ref(['bg-lightGreen', 'bg-blue', 'bg-cherry', 'bg-lightGray'])
+const squaresColors = ref<TIndexedObject<string>>({
+  '#024242': 'bg-darkGreen',
+  '#A5CF4C': 'bg-green',
+  '#D99F59': 'bg-orange',
+  '#889EA6': 'bg-darkGray',
+  '#ffffff': 'bg-white',
+  '#437E60': 'bg-lightGreen',
+  '#4495D1': 'bg-blue',
+  '#DC5973': 'bg-cherry',
+  '#DCDDDE': 'bg-lightGray'
+})
+
+const opacities = ref([100, 80, 60, 40, 20, 15, 10, 5])
 </script>
 
 <style lang="scss">
@@ -48,5 +72,9 @@ const squareSecondaryColors = ref(['bg-lightGreen', 'bg-blue', 'bg-cherry', 'bg-
   font-size: 28px;
   font-weight: 600;
   line-height: 35px;
+}
+
+.white-square-shadow{
+  box-shadow: 0px 4px 14px 0px rgba(0, 0, 0, 0.15);
 }
 </style>
